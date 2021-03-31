d0741153ff34e27d1cab27cf4cb1ee94d8e46d6e
rehmatkarim <p146120@nu.edu.pk>
2021-03-31 12:45:34 -0400

    configured for deployment on heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forcast = require("./utils/forcast");
 
 const app = express();
 
+const port  = process.env.PORT || 3000;
+
 //define paths for express config
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -76,6 +78,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port"+port);
 });
