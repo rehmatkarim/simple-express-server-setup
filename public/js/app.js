38cda75c21ac7b27ca94c53de945b1eb88baec9d
rehmatkarim <p146120@nu.edu.pk>
2021-03-31 10:52:54 -0400

    done

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,23 @@
-console.log("client side javascript.");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.getElementById("message-1");
+const messageTwo = document.getElementById("message-2");
 
-fetch("http://localhost:3000/weather?address=Islamabad").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-      
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
     }
-  });
+  );
 });
